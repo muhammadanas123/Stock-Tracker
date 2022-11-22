f00388a2b7f2ce2b15558a2246932dbbd8896ada
muhammadanas123 <anasfarooqi53@gmail.com>
2022-11-22 14:23:26 +0500

    add the various functionalities

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,15 @@ class Stock < ApplicationRecord
             publishable_token: Rails.application.credentials.iex_client[:iex_api_key],
             secret_token: Rails.application.credentials.iex_client[:secret_token],
             endpoint: 'https://cloud.iexapis.com/v1')
-        client.quote(ticker_symbol).latest_price
+        # client.quote(ticker_symbol).latest_price
+
+        begin
+            new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name,latest_price: client.quote(ticker_symbol).latest_price)
+        rescue => exception
+            return nil
+
+        end
+
+    
     end
 end
