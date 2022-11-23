c7deed6f5535f55b48aa4d33e44f495a1a7d38b0
muhammadanas123 <anasfarooqi53@gmail.com>
2022-11-23 13:22:02 +0500

    Add functionality to remove tracking, modify user model, accept additional fields in edit profile as well as in signup page

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,13 @@
 class Stock < ApplicationRecord
+
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
+    
+    
+
+
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(
             publishable_token: Rails.application.credentials.iex_client[:iex_api_key],
@@ -15,4 +24,9 @@ class Stock < ApplicationRecord
 
     
     end
+
+    def self.check_db(ticker_symbol)
+        where(ticker: ticker_symbol).first
+    end
+
 end
